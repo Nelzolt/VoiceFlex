2a0d584a6d2276a2626194c7d0374c08f9bfbfb6
danielschoder <daniel@schoder.uk>
2024-08-19 08:01:40 +0100

    V10-52

--- VoiceFlex.Tests/BLL/PhoneNumberManagerTests.cs
+++ VoiceFlex.Tests/BLL/PhoneNumberManagerTests.cs
@@ -13,9 +13,10 @@ public class PhoneNumberManagerTests
     private Mock<IAccountAccessor> _mockAccountAccessor;
     private PhoneNumberManager _phoneNumberManager;
     private PhoneNumberDto _phoneNumberDto;
+    private PhoneNumberUpdateDto _phoneNumberUpdateDto;
     private PhoneNumber _phoneNumber;
     private Guid _accountId;
-    private AccountDto _account;
+    private AccountDto _accountDto;
     private Guid _phoneNumberId;
     private Guid _suspendedAccountId;
 
@@ -33,12 +34,13 @@ public class PhoneNumberManagerTests
         {
             Number = "1234"
         };
+        _phoneNumberUpdateDto = new PhoneNumberUpdateDto();
         _phoneNumber = new PhoneNumber
         {
             Id = _phoneNumberId,
             Number = "1234"
         };
-        _account = new AccountDto
+        _accountDto = new AccountDto
         {
             Id = _accountId,
             Status = AccountStatus.Active
@@ -75,6 +77,78 @@ public class PhoneNumberManagerTests
     }
 
     [Test]
+    public async Task AssignUnassignPhoneNumberAsync_Assign_Should_Call_Accessors_And_Validators_With_Correct_Parameters()
+    {
+        // Arrange
+        _phoneNumberUpdateDto.AccountId = _accountId;
+        _mockPhoneNumberAccessor
+            .Setup(accessor => accessor.GetAsync(It.IsAny<Guid>()))
+            .ReturnsAsync(_phoneNumber);
+        _mockAccountAccessor
+            .Setup(accessor => accessor.GetAsync(It.IsAny<Guid>()))
+            .ReturnsAsync(_accountDto);
+
+        _mockPhoneNumberValidator
+            .Setup(v => v.FoundInDatabase(It.IsAny<PhoneNumber>()))
+            .Returns(_mockPhoneNumberValidator.Object);
+        _mockPhoneNumberValidator
+            .Setup(v => v.AccountMustBeInDatabase(It.IsAny<AccountDto>()))
+            .Returns(_mockPhoneNumberValidator.Object);
+        _mockPhoneNumberValidator
+            .Setup(v => v.AccountMustBeActive(It.IsAny<AccountStatus>()))
+            .Returns(_mockPhoneNumberValidator.Object);
+        _mockPhoneNumberValidator
+            .Setup(v => v.PhoneNumberMustBeUnassigned(It.IsAny<Guid?>()))
+            .Returns(_mockPhoneNumberValidator.Object);
+
+        _mockPhoneNumberAccessor
+            .Setup(accessor => accessor.AssignAsync(It.IsAny<PhoneNumber>(), It.IsAny<Guid?>()))
+            .ReturnsAsync(_phoneNumber);
+
+        // Act
+        var assignedPhoneNumber = await _phoneNumberManager.AssignUnassignPhoneNumberAsync(_phoneNumberId, _phoneNumberUpdateDto);
+
+        // Assert
+        _mockPhoneNumberAccessor.Verify(a => a.GetAsync(It.Is<Guid>(p => p.Equals(_phoneNumberId))), Times.Once);
+        _mockAccountAccessor.Verify(a => a.GetAsync(It.Is<Guid>(p => p.Equals(_phoneNumberUpdateDto.AccountId))), Times.Once);
+        _mockPhoneNumberValidator.Verify(a => a.FoundInDatabase(It.Is<PhoneNumber>(p => p.Equals(_phoneNumber))), Times.Once);
+        _mockPhoneNumberValidator.Verify(a => a.AccountMustBeInDatabase(It.Is<AccountDto>(p => p.Equals(_accountDto))), Times.Once);
+        _mockPhoneNumberValidator.Verify(a => a.AccountMustBeActive(It.Is<AccountStatus>(p => p.Equals(_accountDto.Status))), Times.Once);
+        _mockPhoneNumberValidator.Verify(a => a.PhoneNumberMustBeUnassigned(It.Is<Guid?>(p => p.Equals(_phoneNumber.AccountId))), Times.Once);
+        _mockPhoneNumberAccessor.Verify(a => a.AssignAsync(
+            It.Is<PhoneNumber>(p => p.Equals(_phoneNumber)),
+            It.Is<Guid>(p => p.Equals(_phoneNumberUpdateDto.AccountId))), Times.Once);
+        Assert.That(assignedPhoneNumber, Is.EqualTo(_phoneNumber));
+    }
+
+    [Test]
+    public async Task AssignUnassignPhoneNumberAsync_Unassign_Should_Call_Accessors_And_Validators_With_Correct_Parameters()
+    {
+        // Arrange
+        _phoneNumberUpdateDto.AccountId = null;
+        _mockPhoneNumberAccessor
+            .Setup(accessor => accessor.GetAsync(It.IsAny<Guid>()))
+            .ReturnsAsync(_phoneNumber);
+
+        _mockPhoneNumberValidator
+            .Setup(v => v.FoundInDatabase(It.IsAny<PhoneNumber>()))
+            .Returns(_mockPhoneNumberValidator.Object);
+
+        _mockPhoneNumberAccessor
+            .Setup(accessor => accessor.UnassignAsync(It.IsAny<PhoneNumber>()))
+            .ReturnsAsync(_phoneNumber);
+
+        // Act
+        var assignedPhoneNumber = await _phoneNumberManager.AssignUnassignPhoneNumberAsync(_phoneNumberId, _phoneNumberUpdateDto);
+
+        // Assert
+        _mockPhoneNumberAccessor.Verify(a => a.GetAsync(It.Is<Guid>(p => p.Equals(_phoneNumberId))), Times.Once);
+        _mockPhoneNumberValidator.Verify(a => a.FoundInDatabase(It.Is<PhoneNumber>(p => p.Equals(_phoneNumber))), Times.Once);
+        _mockPhoneNumberAccessor.Verify(a => a.UnassignAsync(It.Is<PhoneNumber>(p => p.Equals(_phoneNumber))), Times.Once);
+        Assert.That(assignedPhoneNumber, Is.EqualTo(_phoneNumber));
+    }
+
+    [Test]
     public async Task DeletePhoneNumberAsync_Should_Call_Accessor_And_Validator_With_Correct_Parameters()
     {
         // Arrange
@@ -93,115 +167,4 @@ public class PhoneNumberManagerTests
         _mockPhoneNumberAccessor.Verify(a => a.DeleteAsync(It.Is<Guid>(p => p.Equals(phoneNumberId))), Times.Once);
         Assert.That(deletedPhoneNumber, Is.EqualTo(_phoneNumber));
     }
-
-    //[Test]
-    //public async Task CreatePhoneNumberAsync_Should_Call_PhoneNumberAccessor_CreateAsync_With_Correct_Parameters()
-    //{
-    //    // Arrange
-    //    _mockPhoneNumberAccessor
-    //        .Setup(accessor => accessor.CreateAsync(It.IsAny<PhoneNumberDto>()))
-    //        .ReturnsAsync(_phoneNumber);
-
-    //    // Act
-    //    var actualPhoneNumber = await _phoneNumberManager.CreatePhoneNumberAsync(_expectedPhoneNumber);
-
-    //    // Assert
-    //    _mockPhoneNumberAccessor.Verify(accessor => accessor.CreateAsync(It.Is<PhoneNumberDto>(p => p.Equals(_expectedPhoneNumber))), Times.Once);
-    //    Assert.That(actualPhoneNumber, Is.EqualTo(_phoneNumber));
-    //}
-
-    //[Test]
-    //public async Task CreatePhoneNumberAsync_Should_Call_PhoneNumberValidator_Error_With_Correct_Parameters()
-    //{
-    //    // Arrange
-    //    var mockError = new CallError(ErrorCodes.VOICEFLEX_0001);
-    //    _mockPhoneNumberValidator
-    //        .Setup(v => v.NewPhoneNumberError(It.IsAny<PhoneNumberDto>()))
-    //        .Returns(mockError);
-
-    //    // Act
-    //    var error = await _phoneNumberManager.CreatePhoneNumberAsync(_expectedPhoneNumber) as CallError;
-
-    //    // Assert
-    //    Assert.That(error, Is.Not.Null);
-    //    Assert.That(error.Code, Is.EqualTo(ErrorCodes.VOICEFLEX_0001));
-    //}
-
-    //[Test]
-    //public async Task UpdatePhoneNumberAsync_Should_Call_AccountAccessor_GetAsync_With_Correct_Parameters()
-    //{
-    //    // Arrange
-    //    var phoneNumberId = Guid.NewGuid();
-    //    var phoneNumberUpdateDto = new PhoneNumberUpdateDto { AccountId = _accountId };
-    //    _mockAccountAccessor
-    //        .Setup(accessor => accessor.GetAsync(It.IsAny<Guid>()))
-    //        .ReturnsAsync(_account);
-    //    _mockPhoneNumberAccessor
-    //        .Setup(accessor => accessor.UpdateAsync(It.IsAny<Guid>(), It.IsAny<PhoneNumberUpdateDto>()))
-    //        .ReturnsAsync(_phoneNumber);
-
-    //    // Act
-    //    var phoneNumber = await _phoneNumberManager.AssignPhoneNumberAsync(phoneNumberId, phoneNumberUpdateDto);
-
-    //    // Assert
-    //    _mockAccountAccessor.Verify(accessor => accessor.GetAsync(It.Is<Guid>(p => p.Equals(_accountId))), Times.Once);
-    //}
-
-    //[Test]
-    //public async Task UpdatePhoneNumberAsync_With_Invalid_AccountId_Should_Return_Error()
-    //{
-    //    // Arrange
-    //    AccountDto nullAccount = null;
-    //    var phoneNumberId = Guid.NewGuid();
-    //    var phoneNumberUpdateDto = new PhoneNumberUpdateDto { AccountId = Guid.NewGuid() };
-    //    _mockAccountAccessor
-    //        .Setup(accessor => accessor.GetAsync(It.IsAny<Guid>()))
-    //        .ReturnsAsync(nullAccount);
-
-    //    // Act
-    //    var error = await _phoneNumberManager.AssignPhoneNumberAsync(phoneNumberId, phoneNumberUpdateDto) as CallError;
-
-    //    // Assert
-    //    Assert.That(error, Is.Not.Null);
-    //    Assert.That(error.Code, Is.EqualTo(ErrorCodes.VOICEFLEX_0004));
-    //}
-
-    //[Test]
-    //public async Task UpdatePhoneNumberAsync_With_Suspended_Account_Should_Return_Error()
-    //{
-    //    // Arrange
-    //    var phoneNumberId = Guid.NewGuid();
-    //    var phoneNumberUpdateDto = new PhoneNumberUpdateDto { AccountId = _suspendedAccountId };
-    //    _mockAccountAccessor
-    //        .Setup(accessor => accessor.GetAsync(It.IsAny<Guid>()))
-    //        .ReturnsAsync(_suspendedAccount);
-
-    //    // Act
-    //    var error = await _phoneNumberManager.AssignPhoneNumberAsync(phoneNumberId, phoneNumberUpdateDto) as CallError;
-
-    //    // Assert
-    //    Assert.That(error, Is.Not.Null);
-    //    Assert.That(error.Code, Is.EqualTo(ErrorCodes.VOICEFLEX_0003));
-    //}
-
-    //[Test]
-    //public async Task UpdatePhoneNumberAsync_Should_Call_PhoneNumberAccessor_UpdateAsync_With_Correct_Parameters()
-    //{
-    //    // Arrange
-    //    var phoneNumberId = Guid.NewGuid();
-    //    var phoneNumberUpdateDto = new PhoneNumberUpdateDto();
-    //    _mockPhoneNumberAccessor
-    //        .Setup(accessor => accessor.UpdateAsync(It.IsAny<Guid>(), It.IsAny<PhoneNumberUpdateDto>()))
-    //        .ReturnsAsync(_phoneNumber);
-
-    //    // Act
-    //    var actualPhoneNumber = await _phoneNumberManager.AssignPhoneNumberAsync(phoneNumberId, phoneNumberUpdateDto);
-
-    //    // Assert
-    //    _mockPhoneNumberAccessor.Verify(accessor => accessor.UpdateAsync(
-    //        It.Is<Guid>(p => p.Equals(phoneNumberId)),
-    //        It.Is<PhoneNumberUpdateDto>(p => p.Equals(phoneNumberUpdateDto))),
-    //        Times.Once);
-    //    Assert.That(actualPhoneNumber, Is.EqualTo(_phoneNumber));
-    //}
 }
