34134e10ac700fbd8966532c8f6a774712c1ef5d
danielschoder <daniel@schoder.uk>
2024-08-19 07:20:12 +0100

    V10-52

--- VoiceFlex.Tests/BLL/PhoneNumberManagerTests.cs
+++ VoiceFlex.Tests/BLL/PhoneNumberManagerTests.cs
@@ -12,12 +12,12 @@ public class PhoneNumberManagerTests
     private Mock<IPhoneNumberAccessor> _mockPhoneNumberAccessor;
     private Mock<IAccountAccessor> _mockAccountAccessor;
     private PhoneNumberManager _phoneNumberManager;
-    private PhoneNumberDto _expectedPhoneNumber;
+    private PhoneNumberDto _phoneNumberDto;
     private PhoneNumber _phoneNumber;
     private Guid _accountId;
     private AccountDto _account;
+    private Guid _phoneNumberId;
     private Guid _suspendedAccountId;
-    private AccountDto _suspendedAccount;
 
     [SetUp]
     public void SetUp()
@@ -26,19 +26,72 @@ public class PhoneNumberManagerTests
         _mockPhoneNumberAccessor = new Mock<IPhoneNumberAccessor>();
         _mockAccountAccessor = new Mock<IAccountAccessor>();
         _phoneNumberManager = new PhoneNumberManager(_mockPhoneNumberValidator.Object, _mockPhoneNumberAccessor.Object, _mockAccountAccessor.Object);
-        _expectedPhoneNumber = new PhoneNumberDto();
+        _phoneNumberDto = new PhoneNumberDto();
         _accountId = Guid.NewGuid();
+        _phoneNumberId = Guid.NewGuid();
+        _phoneNumberDto = new PhoneNumberDto
+        {
+            Number = "1234"
+        };
+        _phoneNumber = new PhoneNumber
+        {
+            Id = _phoneNumberId,
+            Number = "1234"
+        };
         _account = new AccountDto
         {
             Id = _accountId,
             Status = AccountStatus.Active
         };
         _suspendedAccountId = Guid.NewGuid();
-        _suspendedAccount = new AccountDto
-        {
-            Id = _suspendedAccountId,
-            Status = AccountStatus.Suspended
-        };
+    }
+
+    [Test]
+    public async Task CreatePhoneNumberAsync_Should_Call_Accessor_And_Validator_With_Correct_Parameters()
+    {
+        // Arrange
+        _mockPhoneNumberAccessor
+            .Setup(accessor => accessor.GetByNumberAsync(It.IsAny<string>()))
+            .ReturnsAsync(_phoneNumber);
+        _mockPhoneNumberValidator
+            .Setup(v => v.NumberMustBeNew(It.IsAny<PhoneNumber>()))
+            .Returns(_mockPhoneNumberValidator.Object);
+        _mockPhoneNumberValidator
+            .Setup(v => v.NumberMustBeValid(It.IsAny<string>()))
+            .Returns(_mockPhoneNumberValidator.Object);
+        _mockPhoneNumberAccessor
+            .Setup(accessor => accessor.CreateAsync(It.IsAny<PhoneNumberDto>()))
+            .ReturnsAsync(_phoneNumber);
+
+        // Act
+        var newPhoneNumber = await _phoneNumberManager.CreatePhoneNumberAsync(_phoneNumberDto);
+
+        // Assert
+        _mockPhoneNumberAccessor.Verify(a => a.GetByNumberAsync(It.Is<string>(p => p.Equals(_phoneNumberDto.Number))), Times.Once);
+        _mockPhoneNumberValidator.Verify(a => a.NumberMustBeNew(It.Is<PhoneNumber>(p => p.Equals(_phoneNumber))), Times.Once);
+        _mockPhoneNumberValidator.Verify(a => a.NumberMustBeValid(It.Is<string>(p => p.Equals(_phoneNumberDto.Number))), Times.Once);
+        _mockPhoneNumberAccessor.Verify(a => a.CreateAsync(It.Is<PhoneNumberDto>(p => p.Equals(_phoneNumberDto))), Times.Once);
+        Assert.That(newPhoneNumber, Is.EqualTo(_phoneNumber));
+    }
+
+    [Test]
+    public async Task DeletePhoneNumberAsync_Should_Call_Accessor_And_Validator_With_Correct_Parameters()
+    {
+        // Arrange
+        var phoneNumberId = Guid.NewGuid();
+        _mockPhoneNumberAccessor
+            .Setup(accessor => accessor.DeleteAsync(It.IsAny<Guid>()))
+            .ReturnsAsync(_phoneNumber);
+        _mockPhoneNumberValidator
+            .Setup(v => v.FoundInDatabase(It.IsAny<PhoneNumber>()))
+            .Returns(_mockPhoneNumberValidator.Object);
+
+        // Act
+        var deletedPhoneNumber = await _phoneNumberManager.DeletePhoneNumberAsync(phoneNumberId);
+
+        // Assert
+        _mockPhoneNumberAccessor.Verify(a => a.DeleteAsync(It.Is<Guid>(p => p.Equals(phoneNumberId))), Times.Once);
+        Assert.That(deletedPhoneNumber, Is.EqualTo(_phoneNumber));
     }
 
     //[Test]
@@ -151,23 +204,4 @@ public class PhoneNumberManagerTests
     //        Times.Once);
     //    Assert.That(actualPhoneNumber, Is.EqualTo(_phoneNumber));
     //}
-
-    [Test]
-    public async Task DeletePhoneNumberAsync_Should_Call_PhoneNumberAccessor_DeleteAsync_With_Correct_Parameters()
-    {
-        // Arrange
-        var phoneNumberId = Guid.NewGuid();
-        _mockPhoneNumberAccessor
-            .Setup(accessor => accessor.DeleteAsync(It.IsAny<Guid>()))
-            .ReturnsAsync(_phoneNumber);
-        _mockPhoneNumberValidator
-            .Setup(v => v.FoundInDatabase(It.IsAny<PhoneNumber>()))
-            .Returns(_mockPhoneNumberValidator.Object);
-
-        // Act
-        await _phoneNumberManager.DeletePhoneNumberAsync(phoneNumberId);
-
-        // Assert
-        _mockPhoneNumberAccessor.Verify(a => a.DeleteAsync(It.Is<Guid>(p => p.Equals(phoneNumberId))), Times.Once);
-    }
 }
